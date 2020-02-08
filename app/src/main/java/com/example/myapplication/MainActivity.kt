94136c83a0f283b198f235c8f708c5fe2d62746e
youngsuk1994 <devyoungsuk>
2020-02-08 19:59:29 +0900

    test

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,5 +10,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         intent
+        intent
+        intent
+        intent
+        intent
     }
 }
